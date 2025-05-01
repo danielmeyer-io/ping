5637171fbdf3c42e8cbe7e1ca2db97769fb5727b
Daniel Meyer <dm@Daniels-MacBook-Pro.local>
2025-05-01 23:22:09 +0200

    init

---
+++ src/test/java/org/example/ping/PingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
